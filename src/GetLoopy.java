46f9c753b97b716df58b94746356a5d6495a6fae
Lab_04_01_SalesTax <ogunleoa@mail.uc.edu>
2025-08-01 02:41:09 -0400

    Task 2 completed

--- src/GetLoopy.java
+++ src/GetLoopy.java
@@ -8,5 +8,11 @@ public class GetLoopy {
         }
         System.out.println("\n"); // new line
 
+        // Task 2: Count down by 1 from 30 to 0
+        System.out.println("Task 2: Count down from 30 to 0");
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
     }
 }
